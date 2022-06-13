91f6cd2fc98518ac35bd7b1bffa878d47bd6f31a
Ario Hartoko <ariohartoko@gmail.com>
2022-06-13 17:53:34 +0700

    Updated office model

--- model/model.go
+++ model/model.go
@@ -8,48 +8,50 @@ type User struct {
 	Email    string `json:"email"`
 	Password string `json:"password,omitempty"`
 	Photourl string `json:"photourl"`
-	number   string `json:"number"`
+	Number   string `json:"number"`
 	//Booking
 }
 
 type Office struct {
-	ID        int `json:"id" gorm:"primaryKey"`
-	CreatedBy int
-	User      User `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
-	TypeID    int
-	//Role Role `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
-	Name        string `json:"name"`
-	Description string `json:"description"`
-	Location    string `json:"location"`
-	ViewCount   int    `json:"view_count"`
-	Price       int    `json:"price"`
-	KursiMin    int    `json:"kursi_min"`
-	KursiMax    int    `json:"kursi_max"`
-	Photourl    string `json:"photourl"`
-	Number      string `json:"number"`
+	ID            int `json:"id" gorm:"primaryKey"`
+	CreatedBy     int
+	User          User `json:"created_by" gorm:"foreignKey:CreatedBy;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	TypeID        int
+	Type          Type           `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	Name          string         `json:"name"`
+	Description   string         `json:"description"`
+	Location      string         `json:"location"`
+	ViewCount     int            `json:"view_count"`
+	Price         int            `json:"price"`
+	KursiMin      int            `json:"kursi_min"`
+	KursiMax      int            `json:"kursi_max"`
+	Photourl      string         `json:"photourl"`
+	Number        string         `json:"number"`
+	Facilitations []Facilitation `gorm:"many2many:office_facilitations;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+	Tags          []Tag          `gorm:"many2many:office_tags;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
 }
 
 type Role struct {
-	Id   int    `json:"id" gorm:"primaryKey"`
-	name string `json:"name"`
+	ID   int    `json:"id" gorm:"primaryKey"`
+	Name string `json:"name"`
 }
 
 type Type struct {
-	Id   int    `json:"id" gorm:"primaryKey"`
-	name string `json:"name"`
+	ID   int    `json:"id" gorm:"primaryKey"`
+	Name string `json:"name"`
 }
 
-type Tags struct {
-	Id   int    `json:"id" gorm:"primaryKey"`
-	name string `json:"name"`
+type Tag struct {
+	ID   int    `json:"id" gorm:"primaryKey"`
+	Name string `json:"name"`
 }
 
-type Facillitations struct {
-	Id   int    `json:"id" gorm:"primaryKey"`
-	name string `json:"name"`
+type Facilitation struct {
+	ID   int    `json:"id" gorm:"primaryKey"`
+	Name string `json:"name"`
 }
 
 type Status struct {
-	Id   int    `json:"id" gorm:"primaryKey"`
-	name string `json:"name"`
+	ID   int    `json:"id" gorm:"primaryKey"`
+	Name string `json:"name"`
 }
