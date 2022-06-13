9f37d01bd20bfbb9d4438df776bdeedb20802276
Ario Hartoko <ariohartoko@gmail.com>
2022-06-13 16:01:05 +0700

    updated model

--- model/model.go
+++ model/model.go
@@ -1,9 +1,30 @@
 package model
 
-type name struct {
-	ID       int    `json:"id" gorm:"primaryKey"`
+type Name struct {
+	ID     int `json:"id" gorm:"primaryKey"`
+	RoleID int
+	//Role Role `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 	Name     string `json:"name"`
-	Email    string `json:"email" form:"email"`
-	Password string `json:"password,omitempty" form:"password"`
-	Photo    string `json:"photo" form:"photo"`
+	Email    string `json:"email"`
+	Password string `json:"password,omitempty"`
+	Photourl string `json:"photourl"`
+	number   string `json:"number"`
+	//Booking
+}
+
+type Office struct {
+	ID        int `json:"id" gorm:"primaryKey"`
+	CreatedBy int
+	//CreatedBy User
+	TypeID int
+	//Role Role `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Location    string `json:"location"`
+	ViewCount   int    `json:"view_count"`
+	Price       int    `json:"price"`
+	KursiMin    int    `json:"kursi_min"`
+	KursiMax    int    `json:"kursi_max"`
+	Photourl    string `json:"photourl"`
+	Number      string `json:"number"`
 }
