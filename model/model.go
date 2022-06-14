a4143ae8365ecca157d9eda83ace5784c67ebca9
Ario Hartoko <ariohartoko@gmail.com>
2022-06-14 12:09:58 +0700

    add bookings model

--- model/model.go
+++ model/model.go
@@ -1,9 +1,11 @@
 package model
 
+import "time"
+
 type User struct {
 	ID       int `json:"id" gorm:"primaryKey"`
 	RoleID   int
-	Role     Role   `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	Role     Role   `json:"role" gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 	Name     string `json:"name"`
 	Email    string `json:"email"`
 	Password string `json:"password,omitempty"`
@@ -17,7 +19,7 @@ type Office struct {
 	CreatedBy     int
 	User          User `json:"created_by" gorm:"foreignKey:CreatedBy;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 	TypeID        int
-	Type          Type           `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	Type          Type           `json:"type" gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 	Name          string         `json:"name"`
 	Description   string         `json:"description"`
 	Location      string         `json:"location"`
@@ -27,8 +29,21 @@ type Office struct {
 	KursiMax      int            `json:"kursi_max"`
 	Photourl      string         `json:"photourl"`
 	Number        string         `json:"number"`
-	Facilitations []Facilitation `gorm:"many2many:office_facilitations;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
-	Tags          []Tag          `gorm:"many2many:office_tags;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+	Facilitations []Facilitation `json:"facilitations" gorm:"many2many:office_facilitations;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+	Tags          []Tag          `json:"tags" gorm:"many2many:office_tags;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+}
+
+type Booking struct {
+	ID              int `json:"id" gorm:"primaryKey"`
+	UserID          int
+	User            User `json:"user" gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	OfficeID        int
+	Office          Office `json:"office" gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	StatusID        int
+	Status          Status    `json:"status" gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	Start           time.Time `json:"start"`
+	End             time.Time `json:"end"`
+	BuktiPembayaran string    `json:"bukti_pembayaran"`
 }
 
 type Role struct {
