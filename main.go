d9eaba2bb556bb7d0b887d5901378d6f0137812e
Ario Hartoko <ariohartoko@gmail.com>
2022-06-16 09:58:43 +0700

    add step by step branching

--- main.go
+++ main.go
@@ -3,3 +3,13 @@ package main
 func main() {
 	//
 }
+
+/*
+
+1. pindah ke branch masing masing
+2. pull dari development (biar dapet yg paling update)
+3. ngerjain masing masing kerjaan di branch
+4. kalo udah push ke branch masing masing tersebut
+5. checkout ke development dan pull dari branch kita tadi lalu push ke development
+
+*/
