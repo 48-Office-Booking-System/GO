abcabe70c301e6a40b153381f0b1adf488ca39c2
Ario Hartoko <ariohartoko@gmail.com>
2022-06-16 10:03:38 +0700

    fixed some typo

--- main.go
+++ main.go
@@ -10,6 +10,8 @@ func main() {
 2. pull dari development (biar dapet yg paling update)
 3. ngerjain masing masing kerjaan di branch
 4. kalo udah push ke branch masing masing tersebut
+
+//bisa diurus sama ario yang no. 5
 5. checkout ke development dan pull dari branch kita tadi lalu push ke development
 
 */
