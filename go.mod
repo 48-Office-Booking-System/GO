d53d05a693270035afc74669e2ef992d82299ba8
Maylida Dwi Chairunnisa <maylidadwi@gmail.com>
2022-06-22 11:46:47 +0700

    updated

--- go.mod
+++ go.mod
@@ -5,6 +5,10 @@ go 1.18
 require gorm.io/gorm v1.23.6
 
 require (
+	github.com/gofrs/uuid v4.0.0+incompatible // indirect
+	github.com/gomodule/redigo v1.8.4 // indirect
+	github.com/googollee/go-socket.io v1.6.2 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.4 // indirect
 )
--- main.go
+++ main.go
@@ -1,7 +1,49 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	socketio "github.com/googollee/go-socket.io"
+)
+
 func main() {
-	//
+	fmt.Println("Hallo Gais")
+
+	// websocket function
+	server, err := socketio.NewServer(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	server.On("connection", func(so socketio.Socket) {
+
+		log.Println("on connection")
+
+		so.Join("chat")
+
+		so.On("chat message", func(msg string) {
+			log.Println("emit:", so.Emit("chat message", msg))
+			so.BroadcastTo("chat", "chat message", msg)
+		})
+
+		so.On("disconnection", func() {
+			log.Println("on disconnect")
+		})
+	})
+
+	server.On("error", func(so socketio.Socket, err error) {
+		log.Println("error:", err)
+	})
+
+	http.Handle("/socket.io/", server)
+
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/", fs)
+
+	log.Println("Serving at localhost:5000...")
+	log.Fatal(http.ListenAndServe(":3007", nil))
 }
 
 /*
